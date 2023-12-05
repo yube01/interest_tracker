24d200410b029dabf62e168d8cb2ece47cab795a
yube01 <yubraja008@gmail.com>
2023-12-05 19:06:35 +0545

    landing page design complete

--- auth/landing.php
+++ auth/landing.php
@@ -3,10 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bank Interest Tracker</title>
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="./style/landing.css">
 </head>
+
 <body>
-    <img src="../assets/logo2.jpg" alt="">
+    <nav>
+        <div class="logo">
+            <a href="landing.php"><img src="../assets/weblogo.png" alt="" ></a>
+        </div>
+        <div class="auth">
+            <a href="login.php"><span>Login</span></a>
+            <a href="register.php"><span>Register</span></a>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="detail">
+                <h1>Bank Interest Tracker</h1>
+                <p>
+                    "Unlock the Power of Precision with Our Bank 
+                    Interest Tracker – Where Every Rate Matters, Empowering 
+                    You to Optimize Your Finances with Informed Decisions."
+                </p>
+        </div>
+        <div class="landimg">
+            <img src="../assets/logo2.jpg" alt="">
+        </div>
+    </div>
+    
+  
 </body>
 </html>
---
+++ auth/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="./style/landing.css">
+    <title>Login</title>
+</head>
+<body>
+<nav>
+        <div class="logo">
+            <a href="landing.php"><img src="../assets/weblogo.png" alt="" ></a>
+        </div>
+        <div class="auth">
+            <a href="login.php"><span>Login</span></a>
+            <a href="register.php"><span>Register</span></a>
+        </div>
+    </nav>
+</body>
+</html>
