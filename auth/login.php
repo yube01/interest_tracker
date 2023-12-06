de1d86cb923a1b768beb8973b28bbe56910a3b47
yube01 <yubraja008@gmail.com>
2023-12-06 20:05:19 +0545

    design part of login and register complete

--- auth/login.php
+++ auth/login.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style.css">
     <link rel="stylesheet" href="./style/landing.css">
+    <link rel="stylesheet" href="./style/register.css">
     <title>Login</title>
 </head>
 <body>
-<nav>
+    <nav>
         <div class="logo">
             <a href="landing.php"><img src="../assets/weblogo.png" alt="" ></a>
         </div>
@@ -17,5 +18,31 @@
             <a href="register.php"><span>Register</span></a>
         </div>
     </nav>
+    <div class="container1">
+
+         <div class="logins">
+            <img src="../assets/logo5.jpg" alt="">
+        </div>
+
+        <div class="register">
+            <h1>Login</h1>
+        <form method="POST">
+            <div class="inputs">
+                <img src="../assets/user.png" alt="">
+                <input required type="text" name="username" placeholder="Username or Email">
+            </div>
+            
+            <div class="inputs">
+            <img src="../assets/password.png" alt="">
+                <input required type="password" name="password" placeholder="Password">
+            </div>
+            <!-- $query = mysql_query(“SELECT * FROM `users` WHERE ‘$name’ IN (username, email) AND `password` = ‘$pass'”) -->
+            
+            <input type="submit" name="submit" class="button" >
+        
+        </form>
+        </div>
+        
+    </div>
 </body>
 </html>
--- auth/register.php
+++ auth/register.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style.css">
     <link rel="stylesheet" href="./style/landing.css">
+    <link rel="stylesheet" href="./style/register.css">
     <title>Register</title>
 </head>
 <body>
-<nav>
+    <nav>
         <div class="logo">
             <a href="landing.php"><img src="../assets/weblogo.png" alt="" ></a>
         </div>
@@ -17,5 +18,33 @@
             <a href="register.php"><span>Register</span></a>
         </div>
     </nav>
+    <div class="container1">
+        <div class="register">
+            <h1>Register</h1>
+        <form method="POST">
+            <div class="inputs">
+                <img src="../assets/user.png" alt="">
+                <input required type="text" name="username" placeholder="Username">
+            </div>
+            <div class="inputs">
+            <img src="../assets/email.png" alt="">
+                <input required type="email" name="email" placeholder="Email">
+            </div>
+            <div class="inputs">
+            <img src="../assets/password.png" alt="">
+                <input required type="password" name="password" placeholder="Password">
+            </div>
+            <div class="inputs">
+            <img src="../assets/password.png" alt="">
+                <input required type="password" name="cpassword" placeholder="Confirm Password">
+            </div>
+            <input type="submit" name="submit" class="button" >
+        
+        </form>
+        </div>
+        <div class="registers">
+            <img src="../assets/logo4.jpg" alt="">
+        </div>
+    </div>
 </body>
 </html>
