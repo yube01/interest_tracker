63bdcb6f2b588a92d7229e64035704738e4ee387
yube01 <yubraja008@gmail.com>
2024-03-14 17:19:20 +0545

    test

---
+++ history/loan.php
@@ -0,0 +1,53 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Loan Calculate History</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="side">
+        <?php
+             include "../Components/sidebar.php"
+        ?>
+        </div>
+        <?php
+        if(isset($_GET['msg'])){
+            ?>
+    <div class="toast" id="toast">
+        <?php
+            
+            $msg =  $_GET['msg'];
+            echo $msg;
+            ?>
+                </div>
+                <?php
+        }
+    ?>
+        <div class="deposit">
+            <div class="sbtn">
+                <?php include "../Components/homenav.php" ?>
+            </div>
+            <div class="first">
+
+                <?php include "../Components/history/loan.php" ?>
+            </div>
+        </div>
+
+        
+       
+        
+    </div>
+    <script>
+    setTimeout(function(){
+        document.getElementById('toast').style.display = 'none';
+    }, 5000); // 4000 milliseconds = 4 seconds
+</script>
+    
+</body>
+</html>
